07201db8421984e55728b68fcb3dfea0be276c9b
Savannah C <sgcorrell@gmail.com>
2019-03-29 16:43:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,30 +9,6 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
